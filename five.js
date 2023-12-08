22102190716bf3726ce1a2bc580cfcee8cb95252
kampit ojha <97059622+Itskampitojha@users.noreply.github.com>
2023-12-08 15:23:50 +0530

    first commit

---
+++ five.js
@@ -0,0 +1,16 @@
+var fullname = "kampit ojha";
+
+var obj = {
+    fullname: "hacked full name",
+
+    prop: {
+        fullname: "InsideProp",
+        getFullname: function(){
+            return this.fullname;
+        },
+    },
+
+    getFullname: function(){
+        
+    }
+}
